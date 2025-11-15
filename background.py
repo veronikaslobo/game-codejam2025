4ec42a374b7043258900ddf99bc0c5108ddc3dc6
Jesus <jesus.rodriguezvisbal@mail.mcgill.ca>
2025-11-15 13:08:52 -0500

    finished bg and deleted old lanes image

--- background.py
+++ background.py
@@ -1,26 +1,43 @@
 import pygame 
+import math
 
 pygame.init()
 
 clock = pygame.time.Clock()
 fps = 60
 
-SCREEN_WIDTH = 1600
+SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+scroll = 0
+targetsize = (SCREEN_WIDTH, SCREEN_HEIGHT)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Game name")
 
+blabala = pygame.image.load(r"C:\Users\danie\Desktop\game-codejam2025\images\Untitled (Draft)-1.png").convert()
+bg = pygame.transform.scale(blabala, targetsize)
+bg_width = bg.get_width()
+bg_height = bg.get_height()
+
+tiles = math.ceil(SCREEN_HEIGHT / bg_width) + 1
+
 run = True
-
-bg = pygame.image.load
-
 while run:
 
     clock.tick(fps)
 
+    # scroll is your vertical offset
+    scroll = (scroll + 5) % bg_height
+
+    # draw two vertical tiles
+    screen.blit(bg, (0, scroll - bg_height))
+    screen.blit(bg, (0, scroll))
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    pygame.display.update()
     
 pygame.quit()
