f7439cc44f239d02e0f444d004194b6d778828e9
veronika <veronika.slobodyan@mail.mcgill.ca>
2025-11-15 10:54:54 -0500

    testing purposes

--- movement.py
+++ movement.py
@@ -4,6 +4,14 @@ import math
 
 pygame.init()
 
+# Test window for car
+WIDTH = 800
+HEIGHT = 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pygame Movement")
+timer = pygame.time.Clock()
+fps = 60
+
 # Class of our car (can be smt else, decide later)
 class Car:
     def __init__(self, x, y, width, height):
